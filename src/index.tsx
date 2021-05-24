22259e24e18a3fbf8162267405e0001a0acd3171
h.sukiasyan <h.sukiasyan@gethatch.com>
2021-05-24 21:28:50 +0200

    create plugin unpkg

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,54 @@
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-  return <h1>Hi</h1>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    // console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </h1>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
