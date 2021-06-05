c1edf971ba252a27de7716085a0644c2bf2bf941
h.sukiasyan <h.sukiasyan@gethatch.com>
2021-06-05 19:24:25 +0200

    add markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom";
+import TextEditor from "./components/text-editor.";
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
